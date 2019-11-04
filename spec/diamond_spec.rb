0dcfa41da7e8a0b08c802172a04b69eed1f19598
Ravenyy <ravenyy@gmail.com>
2019-11-04 10:24:49 +0100

    need to go out already :/

---
+++ spec/diamond_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec/core'
+require_relative '../lib/diamond'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe Diamond do
+  describe '.make_diamond(letter)' do
+    it 'creates proper diamond for A' do
+      expect(Diamond.make_diamond("A")).to eq("A\n")
+    end
+    it 'creates proper diamond out of C' do
+      string =  "  A  \n"\
+                " B B \n"\
+                "C   C\n"\
+                " B B \n"\
+                "  A  \n"
+      expect(Diamond.make_diamond("C")).to eq(string)
+    end
+    it 'creates proper diamont out of E' do
+      string =  "    A    \n"\
+                "   B B   \n"\
+                "  C   C  \n"\
+                " D     D \n"\
+                "E       E\n"\
+                " D     D \n"\
+                "  C   C  \n"\
+                "   B B   \n"\
+                "    A    \n"
+      expect(Diamond.make_diamond("E")).to eq(string)
+    end
+  end
+end
