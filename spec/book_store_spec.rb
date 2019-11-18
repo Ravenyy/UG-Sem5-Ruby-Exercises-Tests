5a4dd9466df3fdee723b40ef8fd0a6fd178408f1
Ravenyy <ravenyy@gmail.com>
2019-11-18 13:01:22 +0100

    minor bugs fixed

--- spec/book_store_spec.rb
+++ spec/book_store_spec.rb
@@ -4,73 +4,74 @@ require 'rspec/core'
 RSpec.describe BookStore do
   describe "Computes offered discount for" do
 
-  it " a single book" do
-    basket = [1]
-    expect(BookStore.calculate_price(basket)).to eq 8.0
-  end
+    it " a single book" do
+      basket = [1]
+      expect(BookStore.calculate_price(basket)).to eq 8.0
+    end
 
-  it " two of the same book" do
-    basket = [2, 2]
-    expect(BookStore.calculate_price(basket)).to eq 16.00
-  end
+    it " two of the same book" do
+      basket = [2, 2]
+      expect(BookStore.calculate_price(basket)).to eq 16.00
+    end
 
-  it " empty basket" do
-    basket = []
-    expect(BookStore.calculate_price(basket)).to eq 0.00
-  end
+    it " empty basket" do
+      basket = []
+      expect(BookStore.calculate_price(basket)).to eq 0.00
+    end
 
-  it " three different books" do
-    basket = [1, 2, 3]
-    expect(BookStore.calculate_price(basket)).to eq 21.60
-  end
+    it " three different books" do
+      basket = [1, 2, 3]
+      expect(BookStore.calculate_price(basket)).to eq 21.60
+    end
 
-  it " four different books" do
-    basket = [1, 2, 3, 4]
-    expect(BookStore.calculate_price(basket)).to eq 25.60
-  end
+    it " four different books" do
+      basket = [1, 2, 3, 4]
+      expect(BookStore.calculate_price(basket)).to eq 25.60
+    end
 
-  it " five different books" do
-    basket = [1, 2, 3, 4, 5]
-    expect(BookStore.calculate_price(basket)).to eq 30.00
-  end
+    it " five different books" do
+      basket = [1, 2, 3, 4, 5]
+      expect(BookStore.calculate_price(basket)).to eq 30.00
+    end
 
-  it " two groups of four is cheaper than group of five plus group of three" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 5]
-    expect(BookStore.calculate_price(basket)).to eq 51.20
-  end
+    it " two groups of four is cheaper than group of five plus group of three" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 5]
+      expect(BookStore.calculate_price(basket)).to eq 51.20
+    end
 
-  it " two groups of four is cheaper than group of five plus group of three" do
-    basket = [1, 1, 2, 3, 4, 4, 5, 5]
-    expect(BookStore.calculate_price(basket)).to eq 51.20
-  end
+    it " two groups of four is cheaper than group of five plus group of three" do
+      basket = [1, 1, 2, 3, 4, 4, 5, 5]
+      expect(BookStore.calculate_price(basket)).to eq 51.20
+    end
 
-  it " two groups of four plus group of two is cheaper than two groups of three" do
-    basket = [1, 1, 2, 2, 3, 4]
-    expect(BookStore.calculate_price(basket)).to eq 40.80
-  end
+    it " two groups of four plus group of two is cheaper than two groups of three" do
+      basket = [1, 1, 2, 2, 3, 4]
+      expect(BookStore.calculate_price(basket)).to eq 40.80
+    end
 
-  it " two each of first 4 books and 1 copy each of rest" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 4, 5]
-    expect(BookStore.calculate_price(basket)).to eq 55.60
-  end
+    it " two each of first 4 books and 1 copy each of rest" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 4, 5]
+      expect(BookStore.calculate_price(basket)).to eq 55.60
+    end
 
-  it " two copies of each book" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-    expect(BookStore.calculate_price(basket)).to eq 60.00
-  end
+    it " two copies of each book" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+      expect(BookStore.calculate_price(basket)).to eq 60.00
+    end
 
-  it " three copies of first book and 2 each of remaining" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
-    expect(BookStore.calculate_price(basket)).to eq 68.00
-  end
+    it " three copies of first book and 2 each of remaining" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
+      expect(BookStore.calculate_price(basket)).to eq 68.00
+    end
 
-  it " three each of first 2 books and 2 each of remaining books" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]
-    expect(BookStore.calculate_price(basket)).to eq 75.20
-  end
+    it " three each of first 2 books and 2 each of remaining books" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]
+      expect(BookStore.calculate_price(basket)).to eq 75.20
+    end
 
-  it " four groups of four are cheaper than two groups each of five and three" do
-    basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]
-    expect(BookStore.calculate_price(basket)).to eq 102.40
+    it " four groups of four are cheaper than two groups each of five and three" do
+      basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]
+      expect(BookStore.calculate_price(basket)).to eq 102.40
+    end
   end
 end
